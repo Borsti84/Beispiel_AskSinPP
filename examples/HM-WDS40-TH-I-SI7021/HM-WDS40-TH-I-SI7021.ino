4351690fa29fc6b1aaf0301284e4a3b4b0da70b0
Borsti84 <43318501+Borsti84@users.noreply.github.com>
2018-10-13 15:20:18 +0200

    Update HM-WDS40-TH-I-SI7021.ino
    Bei kritischen Batteriestand gehe in Sleep Forever Modus

--- examples/HM-WDS40-TH-I-SI7021/HM-WDS40-TH-I-SI7021.ino
+++ examples/HM-WDS40-TH-I-SI7021/HM-WDS40-TH-I-SI7021.ino
@@ -151,8 +151,15 @@ void setup () {
 void loop() {
   bool worked = hal.runready();
   bool poll = sdev.pollRadio();
-  if ( worked == false && poll == false ) {
-    hal.activity.savePower<Sleep<>>(hal);
+   if ( worked == false && poll == false ) {
+    // deep discharge protection
+    // if we drop below critical battery level - switch off all and sleep forever
+    if ( hal.battery.critical() ) {
+      // this call will never return
+      hal.activity.sleepForever(hal);
+    }
+    // if nothing to do - go sleep
+    hal.activity.savePower<Sleep<> >(hal);
   }
 }
 
